285cc6b227cb87aed99e5f557ff03f6448ea67f6
Marcelo Camillo <marcelocamillo18@gmail.com>
2021-12-27 17:26:34 -0300

    Inserindo ID com uuid

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import { categoriesRoutes } from './routes/categories.routes';
 
 const app = express();
 app.use(express.json());
-app.use(categoriesRoutes);
+app.use('/categories', categoriesRoutes);
 
 app.listen(3000, () => {
   console.log('Server is running!');
