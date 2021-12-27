0783827b9452336298098b04e3cd3fa948e68eb2
Marcelo Camillo <marcelocamillo18@gmail.com>
2021-12-27 17:18:00 -0300

    Criando categoria

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
 import express from 'express';
+import { categoriesRoutes } from './routes/categories.routes';
 
 const app = express();
+app.use(express.json());
+app.use(categoriesRoutes);
 
 app.listen(3000, () => {
   console.log('Server is running!');
