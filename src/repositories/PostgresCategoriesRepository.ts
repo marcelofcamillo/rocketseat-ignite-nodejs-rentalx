37ae95176c951cb792036c214af8310375b53231
Marcelo Camillo <marcelocamillo18@gmail.com>
2021-12-30 17:50:48 -0300

    Criando repository de categoria

---
+++ src/repositories/PostgresCategoriesRepository.ts
@@ -0,0 +1,26 @@
+import { Category } from '../model/Category';
+import {
+  ICategoriesRepository,
+  ICreateCategoryDTO,
+} from './ICategoriesRepository';
+
+class PostgresCategoriesRepository implements ICategoriesRepository {
+  findByName(name: string): Category {
+    console.log(name);
+
+    return null;
+    //throw new Error('Method not implemented.');
+  }
+
+  list(): Category[] {
+    return null;
+    //throw new Error('Method not implemented.');
+  }
+
+  create({ name, description }: ICreateCategoryDTO) {
+    console.log(name, description);
+    //throw new Error('Method not implemented.');
+  }
+}
+
+export { PostgresCategoriesRepository };
